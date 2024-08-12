a04067fbdb57e3ff9f56e4f1949e15f473a036e0
gabrielmernandes <gabrielernandes2004@gmail.com>
2024-08-12 17:53:26 +0000

    aula12/08/24

--- app/src/main/java/application/controller/MensagemController.java
+++ app/src/main/java/application/controller/MensagemController.java
@@ -1,8 +1,14 @@
 package application.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class MensagemController {
-    
+    private String[] frutas = {"Maçã", "Banana", "Morango"};
+
+    @GetMapping("/frutas")
+    public String[] list() {
+        return this.frutas;
+    }
 }
